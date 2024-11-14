13d29a7808841dff713d78c24ce8b2f3e89b884a
maryam-ergen <maryam.ergen@mths.ca>
2024-11-14 11:13:46 -0500

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maryam Ergen
+ * Created on: Nov 14
+ * This program shares code via bluetooth
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number
+
+// setup
+radio.setGroup(133)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // checking the distance
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // sending the string
+    if (distanceToObject <= 10) {
+        basic.showIcon(IconNames.No)
+        radio.sendString("Too close")
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+// receiving the string
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
